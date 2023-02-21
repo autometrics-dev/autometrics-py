10f200ba8eadfb09c201376a4c00861db9fc2964
v-aparna <aparna.v.upasana@gmail.com>
2023-02-21 15:15:02 +0000

    changed the example to show both handled and unhandled exceptions

--- src/autometrics.py
+++ src/autometrics.py
@@ -15,7 +15,7 @@ def autometrics(func):
             result = func(*args, **kwargs)
             prom_counter.labels(func_name, module_name, 'ok').inc()
         except Exception as e:
-            result = "exception caught"
+            result = "Exception caught"
             prom_counter.labels(func_name, module_name, 'error').inc()
         duration = time.time() - start_time
         prom_histogram.labels(func_name, module_name).observe(duration)
