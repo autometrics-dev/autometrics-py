82e938ffcd8005a40959e432ad59f8536b430959
v-aparna <aparna.v.upasana@gmail.com>
2023-02-21 10:10:00 +0000

    fixed histogram wrapper

--- src/autometrics.py
+++ src/autometrics.py
@@ -3,28 +3,29 @@ import time
 import inspect
 import functools
 
-function_calls_counter = Counter('function_calls_count', 'query??', ['function', 'module'])
-function_calls_histogram = Histogram('function_calls_duration', 'query??', ['function', 'module'])
-function_calls_gauge = Gauge('function_calls_concurrent', 'query??', ['function', 'module'])
+prom_counter = Counter('function_calls_count', 'query??', ['function', 'module'])
+prom_histogram = Histogram('function_calls_duration', 'query??', ['function', 'module'])
+prom_guage = Gauge('function_calls_concurrent', 'query??', ['function', 'module'])
 
 def function_calls_count(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         func_name = func.__name__
         module_name = inspect.getmodule(func).__name__
-        function_calls_counter.labels(func_name, module_name).inc()
+        prom_counter.labels(func_name, module_name).inc()
         result = func(*args, **kwargs)
         return result
     return wrapper
 
 def function_calls_duration(func):
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
         func_name = func.__name__
         module_name = inspect.getmodule(func).__name__
         start_time = time.time()
         result = func(*args, **kwargs)
         duration = time.time() - start_time
-        function_calls_histogram.labels(func_name, module_name).observe(duration)
+        prom_histogram.labels(func_name, module_name).observe(duration)
         return result
     return wrapper
 
@@ -33,7 +34,7 @@ def function_calls_concurrent(func):
     def wrapper(*args, **kwargs):
         func_name = func.__name__
         module_name = inspect.getmodule(func).__name__
-        function_calls_gauge.labels(func_name, module_name).set(time.time())
+        prom_guage.labels(func_name, module_name).set(time.time())
         result = func(*args, **kwargs)
         return result
     return wrapper
