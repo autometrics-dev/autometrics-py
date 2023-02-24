71e4b07cb8d4b45f7cf729b06fb0766446f09e60
v-aparna <aparna.v.upasana@gmail.com>
2023-02-24 10:36:25 +0000

    module name from func

--- src/autometrics.py
+++ src/autometrics.py
@@ -2,7 +2,6 @@ from prometheus_client import Counter, Histogram, Gauge
 import time
 import inspect
 from prometheus_url import Generator
-import builtins
 import os
 
 prom_counter = Counter('function_calls_count', 'query??', ['function', 'module', 'result'])
@@ -10,17 +9,16 @@ prom_histogram = Histogram('function_calls_duration', 'query??', ['function', 'm
 # prom_guage = Gauge('function_calls_concurrent', 'query??', ['function', 'module']) # we are not doing gauge atm
 
 def autometrics(func):
+    func_name = func.__name__
+    fullname = func.__qualname__
+    filename = get_filename_as_module(func)
+    if fullname == func_name:
+        module_name = filename
+    else:
+        classname = func.__qualname__.split('.')[0]
+        module_name = f"{filename}.{classname}"
     def wrapper(*args, **kwargs):
         func_name = func.__name__
-        filepart = get_filename_as_module(func)
-        if args:
-            class_name = args[0].__class__.__qualname__
-            if class_name in check_if_builtin_type():
-                module_name = filepart
-            else:
-                module_name = f"{filepart}.{class_name}"
-        else:
-            module_name = filepart
         start_time = time.time()
         try:
             result = func(*args, **kwargs)
@@ -30,19 +28,11 @@ def autometrics(func):
             prom_counter.labels(func_name, module_name, 'error').inc()
         duration = time.time() - start_time
         prom_histogram.labels(func_name, module_name).observe(duration)
-        g = Generator(func_name, module_name)
-        urls = g.createURLs()
-        wrapper.__doc__ = f'{func.__doc__}, Prometheus URLs {urls}'
         return result
+    g = Generator(func_name, module_name)
+    urls = g.createURLs()
+    wrapper.__doc__ = f'{func.__doc__}, Prometheus URLs {urls}'
     return wrapper
-
-def check_if_builtin_type():
-    builtin_type = []
-    for name in dir(builtins):
-        fullname = getattr(builtins, name)
-        if isinstance(fullname, type):
-            builtin_type.append(name)
-    return builtin_type
 
 def get_filename_as_module(func):
     fullpath = inspect.getsourcefile(func)
