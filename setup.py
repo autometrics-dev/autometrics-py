0b4e97fcc631542e0ea36e1003731cfc2d37a40e
Aparna Valsala <aparna.v.upasana@gmail.com>
2023-03-22 17:13:48 +0000

    minor corrections

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='autometrics',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'prometheus_client',
