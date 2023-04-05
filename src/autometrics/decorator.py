0bd70f9014174a4e5a81a963087e3fc4970de345
Jacco Flenter <flenter@users.noreply.github.com>
2023-04-05 15:39:48 +0200

    Add SLO support

--- src/autometrics/decorator.py
+++ src/autometrics/decorator.py
@@ -1,21 +1,48 @@
-from collections.abc import Callable
-from prometheus_client import Counter, Histogram, Gauge
-import time
+"""Autometrics module."""
 import inspect
-from .prometheus_url import Generator
 import os
+import time
+from typing import Union
+from collections.abc import Callable
 from functools import wraps
-from typing import Any, TypeVar
+from prometheus_client import Counter, Histogram
+from .constants import (
+    COUNTER_DESCRIPTION,
+    HISTOGRAM_DESCRIPTION,
+    OBJECTIVE_NAME_PROMETHEUS,
+    OBJECTIVE_PERCENTILE_PROMETHEUS,
+    OBJECTIVE_LATENCY_THRESHOLD_PROMETHEUS,
+)
+from .prometheus_url import Generator
+from .objectives import Objective
 
 prom_counter = Counter(
-    "function_calls_count", "query??", ["function", "module", "result", "caller"]
+    "function_calls_count",
+    COUNTER_DESCRIPTION,
+    [
+        "function",
+        "module",
+        "result",
+        "caller",
+        OBJECTIVE_NAME_PROMETHEUS,
+        OBJECTIVE_PERCENTILE_PROMETHEUS,
+    ],
 )
-prom_histogram = Histogram("function_calls_duration", "query??", ["function", "module"])
+prom_histogram = Histogram(
+    "function_calls_duration",
+    HISTOGRAM_DESCRIPTION,
+    [
+        "function",
+        "module",
+        OBJECTIVE_NAME_PROMETHEUS,
+        OBJECTIVE_PERCENTILE_PROMETHEUS,
+        OBJECTIVE_LATENCY_THRESHOLD_PROMETHEUS,
+    ],
+)
 
-R = TypeVar("R")
 
-
-def autometrics(func: Callable) -> Callable:
+def autometrics(func: Callable, objective: Union[None, Objective] = None) -> Callable:
+    """Decorator for tracking function calls and duration."""
     func_name = func.__name__
     fullname = func.__qualname__
     filename = get_filename_as_module(func)
@@ -30,14 +57,44 @@ def autometrics(func: Callable) -> Callable:
         func_name = func.__name__
         start_time = time.time()
         caller = get_caller_function()
+
         try:
             result = func(*args, **kwargs)
-            prom_counter.labels(func_name, module_name, "ok", caller).inc()
-        except Exception as e:
-            result = e.__class__.__name__
-            prom_counter.labels(func_name, module_name, "error", caller).inc()
+            prom_counter.labels(
+                func_name,
+                module_name,
+                "ok",
+                caller,
+                "" if objective is None else objective.name,
+                ""
+                if objective is None or objective.success_rate is None
+                else objective.success_rate,
+            ).inc()
+        except Exception as exception:
+            result = exception.__class__.__name__
+            prom_counter.labels(
+                func_name,
+                module_name,
+                "error",
+                caller,
+                "" if objective is None else objective.name,
+                ""
+                if objective is None or objective.success_rate is None
+                else objective.success_rate,
+            ).inc()
+
         duration = time.time() - start_time
-        prom_histogram.labels(func_name, module_name).observe(duration)
+        prom_histogram.labels(
+            func_name,
+            module_name,
+            "" if objective is None else objective.name,
+            ""
+            if objective is None or objective.latency is None
+            else objective.latency[1],
+            ""
+            if objective is None or objective.latency is None
+            else objective.latency[0],
+        ).observe(duration)
         return result
 
     if func.__doc__ is not None:
@@ -48,25 +105,31 @@ def autometrics(func: Callable) -> Callable:
 
 
 def get_filename_as_module(func: Callable) -> str:
+    """Get the filename of the module that contains the function."""
     fullpath = inspect.getsourcefile(func)
     if fullpath == None:
         return ""
+
     filename = os.path.basename(fullpath)
     module_part = os.path.splitext(filename)[0]
     return module_part
 
 
 def write_docs(func_name: str, module_name: str):
+    """Write the prometheus query urls to the function docstring."""
     g = Generator(func_name, module_name)
-    urls = g.createURLs()
     docs = f"Prometheus Query URLs for Function - {func_name} and Module - {module_name}: \n\n"
+
+    urls = g.createURLs()
     for key, value in urls.items():
         docs = f"{docs}{key} : {value} \n\n"
+
     docs = f"{docs}-------------------------------------------\n"
     return docs
 
 
 def get_caller_function():
+    """Get the name of the function that called the function being decorated."""
     caller_frame = inspect.stack()[2]
     caller_function_name = caller_frame[3]
     return caller_function_name
---
+++ src/autometrics/objectives.py
@@ -0,0 +1,87 @@
+from enum import Enum
+from typing import Union, Tuple
+
+
+class ObjectivePercentile(Enum):
+    """The percentage of requests that must meet the given criteria (success rate or latency)."""
+
+    P90 = "90"
+    P95 = "95"
+    P99 = "99"
+    P99_9 = "99.9"
+
+
+class ObjectiveLatency(Enum):
+    """The latency threshold for the given percentile."""
+
+    Ms5 = "0.005"
+    Ms10 = "0.01"
+    Ms25 = "0.025"
+    Ms50 = "0.05"
+    Ms75 = "0.075"
+    Ms100 = "0.1"
+    Ms250 = "0.25"
+    Ms500 = "0.5"
+    Ms750 = "0.75"
+    Ms1000 = "1"
+    Ms2500 = "2.5"
+    Ms5000 = "5"
+    Ms7500 = "7.5"
+    Ms10000 = "10"
+
+
+# This represents a Service-Level Objective (SLO) for a function or group of functions.
+# The objective should be given a descriptive name and can represent
+# a success rate and/or latency objective.
+#
+# For details on SLOs, see <https://sre.google/sre-book/service-level-objectives/>
+#
+# Example:
+# ```python
+# from autometrics.autometrics import autometrics
+# from autometrics.objective import Objective, ObjectivePercentile, TargetLatency
+# API_SLO: = Objective("api", success_rate=ObjectivePercentile.P99_9, latency = (TargetLatency.Ms250, ObjectivePercentile.P99))
+#
+# @autometrics(objective = API_SLO)]
+# def api_handler() :
+#    # ...
+# ```
+#
+# ## How this works
+#
+# When an objective is added to a function, the metrics for that function will
+# have additional labels attached to specify the SLO details.
+#
+# Autometrics comes with a set of Prometheus [recording rules](https://prometheus.io/docs/prometheus/latest/configuration/recording_rules/)
+# and [alerting rules](https://prometheus.io/docs/prometheus/latest/configuration/alerting_rules/)
+# that will fire alerts when the given objective is being violated.
+#
+# By default, these recording rules will effectively lay dormant.
+# However, they are enabled when the special labels are present on certain metrics.
+class Objective:
+    """A Service-Level Objective (SLO) for a function or group of functions."""
+
+    name: str
+    """name: The name of the objective. This should be something descriptive of the function or group of functions it covers."""
+    success_rate: Union[None, ObjectivePercentile]
+    """Specify the success rate for this objective.
+
+    This means that the function or group of functions that are part of this objective
+    should return an `Ok` result at least this percentage of the time."""
+    latency = Union[None, Tuple[ObjectiveLatency, ObjectivePercentile]]
+
+    def __init__(
+        self,
+        name: str,
+        success_rate: Union[None, ObjectivePercentile] = None,
+        latency: Union[None, Tuple[ObjectiveLatency, ObjectivePercentile]] = None,
+    ):
+        """Create a new objective with the given name.
+
+        The name should be something descriptive of the function or group of functions it covers.
+        For example, if you have an objective covering all of the HTTP handlers in your API you might call it "api".
+        """
+
+        self.name = name
+        self.success_rate = success_rate
+        self.latency = latency
