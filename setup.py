5500b92f214830b244ecf211f743b2f82fc9104a
あで <adelaide@fiberplane.com>
2023-04-05 16:45:11 +0200

    Switch to poetry

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="autometrics",
-    version="0.3",
-    packages=find_packages(),
-    install_requires=["prometheus_client", "python-dotenv"],
-)
