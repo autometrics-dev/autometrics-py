3b110d15763fb05246ab006d587d6655c1b89c51
Brett Beutell <brett@fiberplane.com>
2023-05-09 14:57:05 +0200

    Add support for decorating async functions
    Needs improvement - this is just a copy-pasta

--- src/autometrics/decorator.py
+++ src/autometrics/decorator.py
@@ -1,5 +1,9 @@
 """Autometrics module."""
 import time
+import inspect
+import asyncio
+
+# import asyncio
 from functools import wraps
 from typing import overload, TypeVar, Callable, Optional
 from typing_extensions import ParamSpec
@@ -71,7 +75,52 @@ def autometrics(
             wrapper.__doc__ = f"{func.__doc__}\n{write_docs(func_name, module_name)}"
         return wrapper
 
+    """Decorator for tracking async function calls and duration."""
+
+    def async_decorator(func: Callable[P, T]) -> Callable[P, T]:
+        module_name = get_module_name(func)
+        func_name = func.__name__
+
+        @wraps(func)
+        async def wrapper(*args: P.args, **kwds: P.kwargs) -> T:
+            start_time = time.time()
+            caller = get_caller_function()
+
+            try:
+                result = await func(*args, **kwds)
+                get_tracker().finish(
+                    start_time,
+                    function=func_name,
+                    module=module_name,
+                    caller=caller,
+                    objective=objective,
+                    result=Result.OK,
+                )
+
+            except Exception as exception:
+                result = exception.__class__.__name__
+                get_tracker().finish(
+                    start_time,
+                    function=func_name,
+                    module=module_name,
+                    caller=caller,
+                    objective=objective,
+                    result=Result.ERROR,
+                )
+                # Reraise exception
+                raise exception
+            return result
+
+        if func.__doc__ is None:
+            wrapper.__doc__ = write_docs(func_name, module_name)
+        else:
+            wrapper.__doc__ = f"{func.__doc__}\n{write_docs(func_name, module_name)}"
+        return wrapper
+
     if func is None:
         return decorator
+    elif inspect.iscoroutinefunction(func):
+        print("Using async_decorator")
+        return async_decorator(func)
     else:
         return decorator(func)
