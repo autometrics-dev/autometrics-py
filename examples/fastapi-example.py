23e801e6a806c3a065778a37a98e9b3cc091a1a4
Brett Beutell <brbeut@gmail.com>
2023-05-09 20:57:07 +0200

    Update fastapi-example.py with async code as well

--- examples/fastapi-example.py
+++ examples/fastapi-example.py
@@ -1,3 +1,4 @@
+import asyncio
 from fastapi import FastAPI, Response
 import uvicorn
 from autometrics import autometrics
@@ -21,9 +22,25 @@ def read_root():
     return {"Hello": "World"}
 
 
+# Set up an async handler
+@autometrics
+@app.get("/async")
+async def async_route():
+    message = await do_something_async()
+    return {"Hello": message}
+
+
 @autometrics
 def do_something():
     print("done")
+
+
+@autometrics
+async def do_something_async():
+    print("async start")
+    await asyncio.sleep(2.0)
+    print("async done")
+    return "async world"
 
 
 if __name__ == "__main__":
