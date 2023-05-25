d098676ea9749764d5e2f3eff127ea6692e6cadb
Brett Beutell <brett@fiberplane.com>
2023-05-25 13:15:52 +0200

    Update the HTML we serve

--- src/autometrics/admin_panel/server.py
+++ src/autometrics/admin_panel/server.py
@@ -33,12 +33,17 @@ def run_admin_server():
     server_thread.start()
 
 
-def get_autometrics_admin_html():
-    cdn_root = "http://localhost:8063"
-    css_hash = "48ec065d"
-    js_hash = "a483b9a6"
+def get_autometrics_admin_html(
+    cdn_root=None, css_hash=None, js_hash=None, prom_url=None
+):
+    cdn_root = cdn_root or "https://quickmetrics-ui.pages.dev"
+    css_hash = css_hash or "3527160e"
+    js_hash = js_hash or "24432f8e"
+    prom_url = prom_url or os.getenv("PROMETHEUS_URL")
     return f"""
     <!DOCTYPE html><html lang="en"><head><link rel="stylesheet" href="{cdn_root}/static/css/index.{css_hash}.css"><meta charset="utf-8"><link rel="icon" type="image/svg" href="/favicon.raw.d2e6412d.svg"><meta name="viewport" content="width=device-width, initial-scale=1"><title>Fiberplane</title></head><body> <noscript>You need to enable JavaScript to run this app.</noscript> <div id="root"></div> <textarea id="autometrics-data" style="display:none">
      {json.dumps(get_decorated_functions_list())}
-    </textarea> <script type="module" src="{cdn_root}/static/js/index.{js_hash}.js"></script> </body></html>
+    </textarea>
+    <textarea id="am-prometheus-url" style="display: none" role="hidden">{prom_url}</textarea>
+    <script type="module" src="{cdn_root}/static/js/index.{js_hash}.js"></script> </body></html>
     """
