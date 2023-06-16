dddd5a3aeb2dfa45d7cb9e802ef4dae2590c720b
Brett Beutell <brett@fiberplane.com>
2023-06-16 17:59:43 +0200

    Remove http server for admin panel (old code)

--- src/autometrics/admin_panel/server.py
+++ src/autometrics/admin_panel/server.py
@@ -1,36 +1,7 @@
 import os
-import http.server
-import socketserver
-import threading
-import logging
 import json
 
 from .function_registry import get_decorated_functions_list
-
-
-class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(b"Hello, World!")
-
-
-def _start_admin_server():
-    port = int(os.getenv("AUTOMETRICS_ADMIN_PORT") or "5761")
-    handler = MyHTTPRequestHandler
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
-    logger = logging.getLogger(__name__)
-    with socketserver.TCPServer(("", port), handler) as httpd:
-        logger.info(f"Autometrics admin panel running on http://localhost:{port}")
-        httpd.serve_forever
-
-
-# FIXME - This server dies when called externally
-def run_admin_server():
-    server_thread = threading.Thread(target=_start_admin_server)
-    server_thread.daemon = True
-    server_thread.start()
 
 
 def get_autometrics_admin_html(
