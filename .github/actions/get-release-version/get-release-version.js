2613839c2aabcc4a522ced8b9ed5e22161fc1823
あで <adelaide@fiberplane.com>
2023-08-23 16:21:36 +0200

    Fix version action

--- .github/actions/get-release-version/get-release-version.js
+++ .github/actions/get-release-version/get-release-version.js
@@ -4,7 +4,11 @@ const regex = /release\/([\d.]+)/gm;
 const matches = regex.exec(branchName);
 const version = matches[1];
 if (version) {
-  console.log(`::set-output name=version::${version}`);
+  const fs = require("fs");
+
+  fs.appendFileSync(process.env.GITHUB_STATE, `VERSION=${version}`, {
+    encoding: "utf8",
+  });
 } else {
   throw new Error(`No version found in release title: ${input_title}`);
 }
