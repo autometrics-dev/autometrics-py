445f1e29f4ba550ac2b31d0c7b6e9f498db80930
あで <adelaide@fiberplane.com>
2023-11-17 14:13:51 +0100

    Small fixes for examples & pr template

--- examples/export_metrics/otlp-http.py
+++ examples/export_metrics/otlp-http.py
@@ -1,5 +1,9 @@
 import time
 from autometrics import autometrics, init
+from opentelemetry.sdk.metrics import Counter
+from opentelemetry.sdk.metrics.export import (
+    AggregationTemporality,
+)
 
 # Autometrics supports exporting metrics to OTLP collectors via gRPC and HTTP transports.
 # This example uses the HTTP transport, available settings are similar to the OpenTelemetry
@@ -10,7 +14,14 @@ from autometrics import autometrics, init
 init(
     exporter={
         "type": "otlp-proto-http",
+        "endpoint": "http://localhost:4318/",  # You don't need to set this if you are using the default endpoint
         "push_interval": 1000,
+        # Here are some other available settings:
+        # "timeout": 10,
+        # "headers": {"x-something": "value"},
+        # "aggregation_temporality": {
+        #     Counter: AggregationTemporality.CUMULATIVE,
+        # },
     },
     service_name="otlp-exporter",
 )
